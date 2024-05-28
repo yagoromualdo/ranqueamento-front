145af6d6d9d67079232a14ebba3e87fc70977135
dev.yago <yagoromualdo.yr@gmail.com>
2024-05-28 09:42:50 -0300

    feat: Criação da tela inicial estática
    Realizado: Criação da tela inicial, de listagem de tópicos, de forma estática
    
    Resolve: Épico(KAN-7) -  História(KAN-8)

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,11 +1,114 @@
 import { Component } from '@angular/core';
 
+
+export interface ModelCardInfo {
+  titulo: string,
+  iconPrimeiroLugar: string,
+  primeiroLugar: string,
+  iconSegundoLugar: string,
+  segundoLugar: string,
+  iconTerceiroLugar: string,
+  terceiroLugar: string,
+  qtdVotos: number,
+  qtdComentarios: number
+}
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+
+  listaExemplo: ModelCardInfo[] = [
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+    {
+      titulo: 'Melhor linguagem para trabalhar com IA',
+      iconPrimeiroLugar: 'code',
+      primeiroLugar: 'Linguagem A',
+      iconSegundoLugar: 'code',
+      segundoLugar: 'Linguagem B',
+      iconTerceiroLugar: 'code',
+      terceiroLugar: 'Linguagem C',
+      qtdVotos: 30,
+      qtdComentarios: 10
+    },
+  ];
 
   porVotacao: boolean = true;
 
