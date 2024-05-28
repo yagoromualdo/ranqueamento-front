d65d44afa2ea9fba6ccbd036090a96cf0ffec9b9
Yago Romualdo Vieira <yago.vieira@maximatech.com.br>
2024-05-28 14:13:00 -0300

    feat: Pequeno ajuste - Configurações de Deploy Railway 03
    Realizado: Pequeno ajuste - Configurações de Deploy Railway
    
    Resolve: Épico(KAN-7) -  História(KAN-8)

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const path = require("path");
 
-// Running PORT is set automatically by App Engine
 const port = process.env.PORT || 3000;
 const app = express();
 
-const publicPath = path.join(__dirname, "/dist/front-end");
+const publicPath = path.join(__dirname, "/dist/ranquemaneto-front");
 
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/front-end/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/ranquemaneto-front/index.html"));
 });
-
-// app.listen(process.env.PORT || 8080);
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
